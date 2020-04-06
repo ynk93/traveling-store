ffce648735e3ff8087941ad99294ace18a8150c9
e.yakovlev <eafifa.lover@gmail.com>
2020-04-07 01:43:55 +0300

    basket & checkout pages init

---
+++ wp-content/themes/traveling-store/basket.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+/**
+ * Template Name: FAQ page
+ */
+wp_head(); ?>
+<body>
+<div class="wrapper innerPageHeader">
+
+    <?php get_header(); ?>
+
+    <section class="basketSection">
+
+    </section>
+
+    <?php get_footer(); ?>
+
+</div>
+</body>
+<?php wp_footer(); ?>
+</html>
