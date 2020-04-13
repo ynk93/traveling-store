99775aa85da74e7c7fc22e24bf867a2e04bf2e4f
e.yakovlev <eafifa.lover@gmail.com>
2020-04-13 13:07:57 +0300

    new footer + edits

--- wp-content/themes/traveling-store/footer.php
+++ wp-content/themes/traveling-store/footer.php
@@ -3,66 +3,71 @@
 
         <div class="footerInnerRow">
 
-			<?php $footer_navigation_menu = wp_nav_menu( [
-				'theme_location' => 'footer-navigation',
-				'container'      => false,
-				'menu_class'     => 'menu',
-				'menu_id'        => '',
-				'echo'           => false,
-				'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-				'depth'          => 0,
-			] );
+            <?php $footer_navigation_menu = wp_nav_menu([
+                'theme_location' => 'footer-navigation',
+                'container' => false,
+                'menu_class' => 'menu',
+                'menu_id' => '',
+                'echo' => false,
+                'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                'depth' => 0,
+            ]);
 
-				if ( $footer_navigation_menu ) {
-					echo $footer_navigation_menu;
-				}
+            if ($footer_navigation_menu) {
+                echo $footer_navigation_menu;
+            }
 
-				$contacts_footer_navigation_menu = wp_nav_menu( [
-					'theme_location' => 'contacts-footer-nav',
-					'container'      => false,
-					'menu_class'     => 'menu',
-					'menu_id'        => '',
-					'echo'           => false,
-					'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-					'depth'          => 0,
-				] );
+            $contacts_footer_navigation_menu = wp_nav_menu([
+                'theme_location' => 'contacts-footer-nav',
+                'container' => false,
+                'menu_class' => 'menu',
+                'menu_id' => '',
+                'echo' => false,
+                'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                'depth' => 0,
+            ]);
 
-				if ( $contacts_footer_navigation_menu ) : ?>
+            if ($contacts_footer_navigation_menu) : ?>
 
-                    <div class="contactLinks">
+                <div class="contactLinks">
 
-						<?php echo $contacts_footer_navigation_menu ?>
+                    <?php echo $contacts_footer_navigation_menu ?>
 
-                    </div>
+                </div>
 
-				<?php endif; ?>
+            <?php endif; ?>
 
         </div>
 
         <div class="footerInnerRow">
-
-            <span class="copyRight p">
-                <?php _e( 'Traveling Store 2020 ©. All rights reserved.', 'traveling-store' ); ?>
+            <div class="leftSideWrap">
+                <div class="turSabBlock">
+                    <div class="turSabPic"></div>
+                    <div class="turSabLicense p">License №12521</div>
+                </div>
+                <span class="copyRight p">
+                <?php _e('Traveling Store 2020 ©. All rights reserved.', 'traveling-store'); ?>
             </span>
+            </div>
 
-			<?php $social_footer_navigation_menu = wp_nav_menu( [
-				'theme_location' => 'social-footer-nav',
-				'container'      => false,
-				'menu_id'        => '',
-				'echo'           => false,
-				'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-				'depth'          => 0,
-			] );
+            <?php $social_footer_navigation_menu = wp_nav_menu([
+                'theme_location' => 'social-footer-nav',
+                'container' => false,
+                'menu_id' => '',
+                'echo' => false,
+                'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                'depth' => 0,
+            ]);
 
-				if ( $social_footer_navigation_menu ) : ?>
+            if ($social_footer_navigation_menu) : ?>
 
-                    <div class="socialLinksWrap">
+                <div class="socialLinksWrap">
 
-						<?php echo $social_footer_navigation_menu; ?>
+                    <?php echo $social_footer_navigation_menu; ?>
 
-                    </div>
+                </div>
 
-				<?php endif; ?>
+            <?php endif; ?>
 
         </div>
 
