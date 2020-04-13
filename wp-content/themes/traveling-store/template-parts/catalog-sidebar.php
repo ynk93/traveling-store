fb25aaf53b512c70ed708c2ff09ae0966fd5b9e7
Bohdan Pozdniakov <pz.bohdan@gmail.com>
2020-04-13 12:02:39 +0300

    UPD: Catalog loop + cart init

---
+++ wp-content/themes/traveling-store/template-parts/catalog-sidebar.php
@@ -0,0 +1,110 @@
+<div class="catalogSideBarWrap">
+
+	<div class="sideBarBlock">
+		<div class="sideBarHead">
+			<div class="h5"><?php _e('По категориям','traveling-store'); ?></div>
+		</div>
+		<div class="sideBarBody">
+			<div class="chbWrap">
+				<label class="container">
+					<input type="checkbox" checked="checked">
+					<span class="checkmark"></span>
+					<span class="chbLabel">Показать все</span>
+				</label>
+			</div>
+			<div class="chbWrap">
+				<label class="container">
+					<input type="checkbox">
+					<span class="checkmark"></span>
+					<span class="chbLabel">Экскурсии</span>
+				</label>
+			</div>
+			<div class="chbWrap">
+				<label class="container">
+					<input type="checkbox">
+					<span class="checkmark"></span>
+					<span class="chbLabel">Рестораны</span>
+				</label>
+			</div>
+			<div class="chbWrap">
+				<label class="container">
+					<input type="checkbox">
+					<span class="checkmark"></span>
+					<span class="chbLabel">Шопинг</span>
+				</label>
+			</div>
+			<div class="chbWrap">
+				<label class="container">
+					<input type="checkbox">
+					<span class="checkmark"></span>
+					<span class="chbLabel">VIP</span>
+				</label>
+			</div>
+		</div>
+	</div>
+
+	<div class="sideBarBlock">
+		<div class="sideBarHead">
+			<div class="h5"><?php _e('По регионам', 'traveling-store'); ?></div>
+		</div>
+		<div class="sideBarBody">
+			<div class="chbWrap">
+				<label class="container">
+					<input type="checkbox" checked="checked">
+					<span class="checkmark"></span>
+					<span class="chbLabel">Показать все</span>
+				</label>
+			</div>
+			<div class="chbWrap">
+				<label class="container">
+					<input type="checkbox">
+					<span class="checkmark"></span>
+					<span class="chbLabel">Анталия</span>
+				</label>
+			</div>
+			<div class="chbWrap">
+				<label class="container">
+					<input type="checkbox">
+					<span class="checkmark"></span>
+					<span class="chbLabel">Кемер</span>
+				</label>
+			</div>
+			<div class="chbWrap">
+				<label class="container">
+					<input type="checkbox">
+					<span class="checkmark"></span>
+					<span class="chbLabel">Фетхие</span>
+				</label>
+			</div>
+			<div class="chbWrap">
+				<label class="container">
+					<input type="checkbox">
+					<span class="checkmark"></span>
+					<span class="chbLabel">Алания</span>
+				</label>
+			</div>
+			<div class="chbWrap">
+				<label class="container">
+					<input type="checkbox">
+					<span class="checkmark"></span>
+					<span class="chbLabel">Мармарис</span>
+				</label>
+			</div>
+			<div class="chbWrap">
+				<label class="container">
+					<input type="checkbox">
+					<span class="checkmark"></span>
+					<span class="chbLabel">Белек</span>
+				</label>
+			</div>
+			<div class="chbWrap">
+				<label class="container">
+					<input type="checkbox">
+					<span class="checkmark"></span>
+					<span class="chbLabel">Сиде</span>
+				</label>
+			</div>
+		</div>
+	</div>
+
+</div>
