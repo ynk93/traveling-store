3af4e14d5a4b1969dce2d955f7ba1998d2bf9cf1
Bohdan Pozdniakov <pz.bohdan@gmail.com>
2020-04-20 17:15:58 +0300

    UPD: Tours page (without pagination + tour card info not done)

--- wp-content/themes/traveling-store/woocommerce/archive-product.php
+++ wp-content/themes/traveling-store/woocommerce/archive-product.php
@@ -1,10 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-/**
- * Template Name: Shop page
- */
-wp_head(); ?>
+	global $wp_query;
+
+	/**
+	 * Template Name: Tours page
+	 */
+	wp_head();
+
+	$term = $wp_query->get_queried_object();
+
+	var_dump($term);
+?>
 <body>
 <div class="wrapper innerPageHeader">
 
@@ -14,7 +21,7 @@ wp_head(); ?>
         <div class="content">
 
             <div class="titleWrap innerPageTitleWrap">
-                <div class="h2"><?php _e('Туры', 'traveling-store'); ?></div>
+                <div class="h2"><?php _e('Tours', 'traveling-store'); ?></div>
             </div>
 
             <div class="catalogSectionRow">
@@ -34,6 +41,39 @@ wp_head(); ?>
 			                'product_tag'    => get_query_var( 'product_tag' ) ? get_query_var( 'product_tag' ) : '',
 			                'tax_query'      => array()
 		                );
+
+		                $args = array_merge( $args, wp_parse_args( $_SERVER['QUERY_STRING'] ) );
+
+		                if ( ! count( wp_parse_args( $_SERVER['QUERY_STRING'] ) ) && ( is_product_category() || is_product_tag() || taxonomy_is_product_attribute($term->taxonomy) ) ) {
+			                $param = get_queried_object();
+
+			                echo '<script> var initQs={' . $param->taxonomy . ':"' . $param->slug . '"}</script>';
+			                $args[ $param->taxonomy ] = $param->slug;
+		                } else {
+			                echo '<script> var initQs={"product_cat":"' . $args['product_cat'] . '", "product_tag":"' . $args['product_tag'] . '", "product_type":"' . $args['product_type'] . '", "pa_regions":"' . $args['pa_regions'] . '"}</script>';
+		                }
+
+		                if ( ! isset( $args['order'] ) ) {
+			                $args = array_merge( $args, array(
+				                'orderby' => 'menu_order+title',
+				                'order'   => 'ASC',
+			                ) );
+		                } else {
+			                $args['orderby'] = 'meta_value_num';
+		                }
+
+		                if ( ! isset( $args['tax_query'] ) ) {
+			                $args['tax_query'] = array();
+		                }
+
+		                $args['tax_query'][] = array(
+			                'taxonomy' => 'product_visibility',
+			                'terms'    => array( 'exclude-from-catalog', 'hidden' ),
+			                'field'    => 'slug',
+			                'operator' => 'NOT IN',
+		                );
+
+		                var_dump($args);
 
 		                $loop = new WP_Query( $args );
 
