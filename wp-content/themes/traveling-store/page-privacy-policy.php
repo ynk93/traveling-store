680ddab3e057d65c85c99ce23cc00a5426078376
Bohdan Pozdniakov <pz.bohdan@gmail.com>
2020-05-02 17:56:18 +0300

    UPD: Page privacy policy

---
+++ wp-content/themes/traveling-store/page-privacy-policy.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+	/**
+	 * Template Name: Privacy policy page
+	 */
+	wp_head(); ?>
+<body>
+<div class="wrapper innerPageHeader">
+
+	<?php get_header(); ?>
+    <section class="innerPageSection faqPageSection">
+        <div class="content">
+            <div class="titleWrap innerPageTitleWrap">
+                <div class="h2"><?php the_title(); ?></div>
+            </div>
+
+			<?php
+				while ( have_posts() ) : the_post(); ?>
+
+                    <div class="textPageContent">
+						<?php the_content(); ?>
+                    </div>
+
+				<?php
+				endwhile;
+				wp_reset_query();
+			?>
+
+        </div>
+    </section>
+
+	<?php get_footer(); ?>
+
+</div>
+</body>
+<?php wp_footer(); ?>
+</html>
